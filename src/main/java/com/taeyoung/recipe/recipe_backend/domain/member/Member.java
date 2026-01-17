d5c95913c43492f1aac2b60b94199daac97eadc0
LeeTaeYoung <xodud5080@naver.com>
2026-01-17 10:36:48 +0900

    merge social-login

--- src/main/java/com/taeyoung/recipe/recipe_backend/domain/member/Member.java
+++ src/main/java/com/taeyoung/recipe/recipe_backend/domain/member/Member.java
@@ -9,12 +9,12 @@ import java.time.LocalDate;
 @Entity
 @Getter
 @Table(
-    name = "member",
-    uniqueConstraints = {
-        @UniqueConstraint(
-            columnNames = {"provider", "provider_id"}
-        )
-    }
+        name = "member",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        columnNames = {"provider", "provider_id"}
+                )
+        }
 )
 public class Member extends BaseEntity {
 
@@ -27,15 +27,16 @@ public class Member extends BaseEntity {
 
     @Column(name = "provider_id")
     private String providerId;
-            
+
+    // 연동 대상 (LOCAL 계정의 member_id)
+    @Column(name = "linked_member_id")
+    private Long linkedMemberId;
+
     @Enumerated(EnumType.STRING)
     private Role role;
 
     @Column(unique = true)
     private String username;
-
-//    @Column(unique = true)
-//    private String email;
 
     private String password;
     private String name;
@@ -90,6 +91,15 @@ public class Member extends BaseEntity {
 
     public void setRole(Role role){
         this.role = role;
+    }
+
+    public static Member createSocialMember(String username, ProviderType provider, String providerId, Role role) {
+        Member member = new Member();
+        member.username = username;
+        member.provider = provider;
+        member.providerId = providerId;
+        member.role = role;
+        return member;
     }
 }
 
