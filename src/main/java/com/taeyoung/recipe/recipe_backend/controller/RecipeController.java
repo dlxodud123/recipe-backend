016da1a8cc48548c5ef7b57992636ecfa0e2f7aa
LeeTaeYoung <xodud5080@naver.com>
2026-01-24 11:44:33 +0900

    check request data 10

--- src/main/java/com/taeyoung/recipe/recipe_backend/controller/RecipeController.java
+++ src/main/java/com/taeyoung/recipe/recipe_backend/controller/RecipeController.java
@@ -27,21 +27,18 @@ public class RecipeController {
     public ResponseEntity<String> createStudy(@RequestPart("image") MultipartFile image,
                                          @Valid @RequestPart("recipe") RecipeCreateRequestDto recipeCreateRequestDto,
                                          Authentication authentication) throws IOException {
-        Long userId = ((CustomUser) authentication.getPrincipal()).getId();
+//        Long userId = ((CustomUser) authentication.getPrincipal()).getId();
 
         if (image == null || image.isEmpty()) {
             throw new IllegalArgumentException("이미지를 업로드해주세요");
         }
 
+        String imageUrl = s3UploaderService.uploadFile(image);
+
         // 서버용
-//        Recipe saved = recipeService.save(recipeCreateRequestDto, imageUrl, userId);
-        // 테스트용
-//        Recipe saved = recipeService.save(recipeCreateRequestDto, imageUrl, 7L);
-//
+//        recipeService.save(recipeCreateRequestDto, imageUrl, userId);
 //        return ResponseEntity.ok(saved);
 
-//        String imageUrl = "레시피 저장 완료!";
-        String imageUrl = s3UploaderService.uploadFile(image);
 
         recipeService.save(recipeCreateRequestDto, imageUrl, 7L);
 
