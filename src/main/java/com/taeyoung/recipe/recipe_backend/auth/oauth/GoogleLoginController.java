d5dc88992b16969b7f0f7f47955ada462b7e2847
LeeTaeYoung <xodud5080@naver.com>
2026-02-02 14:52:02 +0900

    modify local google

--- src/main/java/com/taeyoung/recipe/recipe_backend/auth/oauth/GoogleLoginController.java
+++ src/main/java/com/taeyoung/recipe/recipe_backend/auth/oauth/GoogleLoginController.java
@@ -55,7 +55,8 @@ public class GoogleLoginController {
         sendJwtToResponse(member, response);
 
         // 뷰 반환 없이 JSON 응답만
-        return "redirect:https://mealhub.site";
+//        return "redirect:https://mealhub.site";
+        return "redirect:http://localhost:3000";
     }
 
     // JWT 생성 + 쿠키 설정 + JSON 응답
