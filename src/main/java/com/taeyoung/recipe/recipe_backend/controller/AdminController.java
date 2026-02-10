877338f1e53ae54cd047626e022c57dc7d1f8078
LeeTaeYoung <xodud5080@naver.com>
2026-02-10 12:56:55 +0900

    modify adminController

--- src/main/java/com/taeyoung/recipe/recipe_backend/controller/AdminController.java
+++ src/main/java/com/taeyoung/recipe/recipe_backend/controller/AdminController.java
@@ -14,7 +14,7 @@ import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
-@PreAuthorize("hasRole('ADMIN')")
+//@PreAuthorize("hasRole('ADMIN')")
 @RequestMapping("/api/admin")
 public class AdminController {
 
